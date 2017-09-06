1dd6155f70e90d59d467073ee63b0b1ae82d0fb1
Tom DeFrank <tom@thinkargent.com>
2017-09-06 14:36:40 -0400

    profile page layout changes

--- src/components/User/Profile.vue
+++ src/components/User/Profile.vue
@@ -4,31 +4,41 @@
       v-flex(xs12, sm10, md8)
         v-card(v-if="currentUser")
           v-card-title(class="primary white--text mb-0 pa-3 headline") Your Profile
-          v-card-title
-            span(class="headline") {{`${currentUser.firstname} ${currentUser.lastname}`}}
-            span(class="subtitle ml-2") {{`(${currentUser.email})`}}
           v-card-text
-            div(v-if="!currentUser.registeredMeetups") You are not registered for any meetups.
-            div(v-else class="title") Registered Meetups:
-            v-list(v-if="currentUser.registeredMeetups" three-line)
-              v-list-tile(v-for="(meetup, index) in getMeetups(currentUser.registeredMeetups)" :key="meetup.id" :to="'/meetup/' + meetup.id")
-                v-list-tile-content
-                    v-list-tile-title {{meetup.title}}
-                    v-list-tile-sub-title {{ meetup.location }}
-                    v-list-tile-sub-title {{ dt(meetup.datetime) }}
-                v-list-tile-action
-                  v-dialog(v-model="dialog")
-                    v-icon(slot="activator" style="cursor:pointer" class="red--text" @click="selectedUnregMeetupTitle = meetup.title; selectedUnregMeetupID = meetup.id") cancel
-                    v-card
-                      v-card-title(class="headline red white--text px-3 py-2", style="display:block")
-                        | Unregister?
-                      v-card-text
-                        | This will unregister you from {{selectedUnregMeetupTitle}}.
-                      v-card-actions
-                        v-spacer
-                        v-btn(class="red--text", flat="flat" @click.native="dialog = false") Cancel
-                        v-btn(class="red--text", flat="flat" @click.native="unregisterMeetup(selectedUnregMeetupID)") Do It
-</template>
+            v-layout(row, wrap)
+              v-flex(xs4)
+                v-list
+                  v-list-tile
+                    v-icon(class="mr-2") person
+                    span {{`${currentUser.firstname} ${currentUser.lastname}`}}
+                  v-list-tile
+                    v-icon(class="mr-2") mail
+                    span {{`(${currentUser.email})`}}
+              v-flex(xs8)
+                div(v-if="!currentUser.registeredMeetups") You are not registered for any meetups.
+                v-card(v-if="currentUser.registeredMeetups")
+                  v-card-title(class="title primary white--text pa-2") Registered Meetups:
+                  v-card-text(class="py-2")
+                    v-layout(v-for="(meetup, index) in getMeetups(currentUser.registeredMeetups)" :key="meetup.id" pa-2 mt-1 justify-center align-center style="background:#f0f0f0")
+                      div
+                        router-link(:to="'/meetup/' + meetup.id")
+                          div {{meetup.title}}
+                          div {{ meetup.location }}
+                          div {{ dt(meetup.datetime) }}
+                      v-spacer
+                      div
+                        v-dialog(v-model="dialog")
+                          v-icon(slot="activator" style="cursor:pointer" class="red--text" @click="selectedUnregMeetupTitle = meetup.title; selectedUnregMeetupID = meetup.id") cancel
+                          v-card
+                            v-card-title(class="headline red white--text px-3 py-2", style="display:block")
+                              | Unregister?
+                            v-card-text
+                              | This will unregister you from {{selectedUnregMeetupTitle}}.
+                            v-card-actions
+                              v-spacer
+                              v-btn(class="red--text", flat="flat" @click.native="dialog = false") Cancel
+                              v-btn(class="red--text", flat="flat" @click.native="unregisterMeetup(selectedUnregMeetupID)") Do It
+  </template>
 
 <script>
   import moment from 'moment'
@@ -51,8 +61,10 @@
     },
     methods: {
       getMeetups(mus){
-        var arr = []
-        mus.forEach(i => arr.push(this.meetups.find(m => i === m.id)))
+        if(mus){
+          var arr = []
+          mus.forEach(i => arr.push(this.meetups.find(m => i === m.id)))
+        }
         return arr
       },
       dt(datetime){
